70f03823d674d3c7b016b60a370860fac30a99ee
Alejandro Campos Jimeno <camposjimeno@gmail.com>
2020-10-28 00:04:16 +0100

    Hecho

--- Tile.py
+++ Tile.py
@@ -1,31 +1,24 @@
 class Tile:
     
-    endX = 0
-    endY = 0
-    X = 0
-    Y = 0
-    
-    tileNumber = 0
-    
-    avMove = "none"
-    movesToEnd = 0
-    
-    
+ 
+
     def __init__(self, tileNum, endX, endY , X, Y):
         self.X = X
         self.Y = Y
         self.endX = endX
         self.endY = endY
         self.tileNumber = tileNum
+        self.avMove = "none"
+        self.movesToEnd = 0
         
     def move(self):
         
         if self.avMove == "up":
-            self.Y += 1
+            self.Y -= 1
             return
             
         if self.avMove == "down":
-            self.Y -= 1
+            self.Y += 1
             return
             
         if self.avMove == "right":
@@ -35,7 +28,7 @@ class Tile:
         if self.avMove == "left":
             self.X -= 1
             return
-            
+        
         return
         
     def getX(self):
@@ -45,7 +38,7 @@ class Tile:
         return self.Y
     
     def getMovesToEnd(self):
-        return (abs(self.endX - self.X) + abs(self.endY - self.Y))
+        return int((abs(self.endX - self.X) + abs(self.endY - self.Y)))
     
     def setAvMove(self, move):
         self.avMove = move
@@ -67,11 +60,11 @@ class Tile:
             return self.avMove
             
         if (((self.Y - tile.getY()) == 1) & (self.X == tile.getX())):
-            self.avMove="down"
+            self.avMove="up"
             return self.avMove
             
         if (((self.Y - tile.getY()) == -1) & (self.X == tile.getX())):
-            self.avMove="up"
+            self.avMove="down"
             return self.avMove
         
         self.avMove = "none"
@@ -79,35 +72,35 @@ class Tile:
         
     def getTempMovesToEnd(self):
         if (self.avMove == "right"):
-            return ((abs(self.endX - self.X + 1)) + abs(self.endY - self.Y))
+            return ((abs(self.endX - self.X + 1) + abs(self.endY - self.Y)))
         
         if (self.avMove == "left"):
-            return ((abs(self.endX - self.X - 1)) + abs(self.endY - self.Y))
+            return ((abs(self.endX - self.X - 1) + abs(self.endY - self.Y)))
         
         if (self.avMove == "up"):
-            return ((abs(self.endX - self.X)) + abs(self.endY - self.Y - 1))
+            return ((abs(self.endX - self.X) + abs(self.endY - self.Y + 1)))
         
         if (self.avMove == "down"):
-            return ((abs(self.endX - self.X + 1)) + abs(self.endY - self.Y + 1))
+            return ((abs(self.endX - self.X) + abs(self.endY - self.Y - 1)))
 
     def getPosition(self):
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 0) & (self.Y == 0)):
             return 1
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 1) & (self.Y == 0)):
             return 2
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 2) & (self.Y == 0)):
             return 3
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 2) & (self.Y == 1)):
             return 4
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 2) & (self.Y == 2)):
             return 5
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 1) & (self.Y == 2)):
             return 6
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 0) & (self.Y == 2)):
             return 7
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 0) & (self.Y == 1)):
             return 8
-        if ((x == 0) & (y == 0)):
+        if ((self.X == 1) & (self.Y == 1)):
             return 9
 
         return
--- TilesBoard.py
+++ TilesBoard.py
@@ -1,75 +1,114 @@
-
+import copy
 from Tile import Tile
 
 class TilesBoard:
     
-    tile1 = Tile(1, 0, 0, 2, 0)
-    tile2 = Tile(2, 1, 0, 1, 0)
-    tile3 = Tile(3, 2, 0, 0, 2)
-    tile4 = Tile(4, 2, 1, 2, 1)
-    tile5 = Tile(5, 2, 2, 2, 2)
-    tile6 = Tile(6, 1, 2, 0, 1)
-    tile7 = Tile(7, 0, 2, 1, 1)
-    tile8 = Tile(8, 0, 1, 1, 2)
-    
-    emptyTile = Tile(0, 1, 1, 0, 0)
-    
-    tiles = [emptyTile, tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8]
+    def __init__ (self):
 
-    movements = 0
-    pastStates = []
+        self.tile1 = Tile(1, 0, 0, 2, 0)
+        self.tile2 = Tile(2, 1, 0, 1, 0)
+        self.tile3 = Tile(3, 2, 0, 0, 2)
+        self.tile4 = Tile(4, 2, 1, 2, 1)
+        self.tile5 = Tile(5, 2, 2, 2, 2)
+        self.tile6 = Tile(6, 1, 2, 0, 1)
+        self.tile7 = Tile(7, 0, 2, 1, 1)
+        self.tile8 = Tile(8, 0, 1, 1, 2)
+    
+        self.emptyTile = Tile(0, 1, 1, 0, 0)
+    
+        self.tiles = [self.emptyTile, self.tile1, self.tile2, self.tile3, self.tile4, self.tile5, self.tile6, self.tile7, self.tile8]
+
+        self.movements = 0
+        self.pastStates = []
+
     
     def moveTiles(self):
         
         avMoves = []
         movableTiles = []
         i = 0
+        k = 0
+        x = 0
 
         tmpState = []
+        tmpTiles = []
+        possibleStates = []
         
         totalMovesToEnd = 0
         
-        for ti in self.tiles:
-            if (ti.getNumber() == 0):
+        for ti in self.tiles: # Checking which tiles are able to me moved (is adyacent to empty space)
+            if (ti.getNumber() == 0): # Not checking for the space
                 continue
-            totalMovesToEnd += ti.getMovesToEnd()
-            if (ti.canMove(self.emptyTile) != "none"):
-                movableTiles.append(ti)
+            totalMovesToEnd += ti.getMovesToEnd() # Using the loop to get the current moves left to win
+            if (ti.canMove(self.tiles[0]) != "none"): # The tile class will remember if and where it can be moved
+                movableTiles.append(ti) # Adding the movable tile to a list
+        
+        while x < len(movableTiles): # Checking if a move leads to a past state of the tile board
+            for y in range(len(self.pastStates)):
+                if ((self.getPossibleState(movableTiles[x].getNumber())) == self.pastStates[y]):
+                    movableTiles.pop(x) # Discarding it
+                    x -= 1
+                    break
             
-        for ti in movableTiles:
+            x += 1
+
+        for ti in movableTiles: # A new list with the moves left to win for every possible move
             avMoves.append(totalMovesToEnd - ti.getMovesToEnd() + ti.getTempMovesToEnd())
         
-        for k in range((len(movableTiles))-1):
+        for k in range((len(movableTiles))-1): # Checking which move is best taking the one with the least moves to win
             if (movableTiles[k+1].getMovesToEnd() < movableTiles[k].getMovesToEnd()):
                 i = k+1
 
-
-             
         if (movableTiles[i].getAvMove() == "up"):
-            self.emptyTile.setAvMove("down")
-        else:
-            self.emptyTile.setAvMove("up")
+            self.tiles[0].setAvMove("down")
+        
+        if (movableTiles[i].getAvMove() == "down"):
+            self.tiles[0].setAvMove("up")
                 
         if (movableTiles[i].getAvMove() == "right"):
-            self.emptyTile.setAvMove("left")
-        else:
-            self.emptyTile.setAvMove("right")
+            self.tiles[0].setAvMove("left")
+
+        if (movableTiles[i].getAvMove() == "left"):
+            self.tiles[0].setAvMove("right")
         
-        self.emptyTile.move()
+        self.tiles[0].move()
         self.tiles[movableTiles[i].getNumber()].move()
 
 
         for tile in self.tiles:
-            tmpState.append(tile.getPosition)
-
-        #if (tmpState == self.pastStates[movements]):
-        #    movableTiles.pop(i)
+            tmpState.append(tile.getPosition())
+            self.tiles[tile.getNumber()].setAvMove("none")
 
         self.movements += 1
         self.pastStates.append(tmpState)
 
         return
+       
+    def getPossibleState(self, tileNumber):
+        state = []
+        tmpTiles = copy.deepcopy(self.tiles)
+        if (tmpTiles[tileNumber].getAvMove() == "up"):
+            tmpTiles[0].setAvMove("down")
         
+        if (tmpTiles[tileNumber].getAvMove() == "down"):
+            tmpTiles[0].setAvMove("up")
+                
+        if (tmpTiles[tileNumber].getAvMove() == "right"):
+            tmpTiles[0].setAvMove("left")
+        
+        if (tmpTiles[tileNumber].getAvMove() == "left"):
+            tmpTiles[0].setAvMove("right")
+
+        
+        tmpTiles[tileNumber].move()
+        tmpTiles[0].move()
+
+        for ti in tmpTiles:
+            state.append(ti.getPosition())
+
+        tmpTiles.clear()
+        return state
+
     def printBoard(self):
         x = 0
         y = 0
@@ -78,7 +117,6 @@ class TilesBoard:
             y = tile.getY()
             if ((x == 0) & (y == 0)):
                 print("\n|" + str(tile.getNumber()), end = " ")
-                break
         for tile in (self.tiles):
             x = tile.getX()
             y = tile.getY()        
@@ -88,12 +126,12 @@ class TilesBoard:
             x = tile.getX()
             y = tile.getY()        
             if ((x == 2) & (y == 0)):
-                print("|" + str(tile.getNumber()), end = " ")
+                print("|" + str(tile.getNumber()) + "|\n", end = " ")
         for tile in (self.tiles):
             x = tile.getX()
             y = tile.getY()        
             if ((x == 0) & (y == 1)):
-                print("|\n|" + str(tile.getNumber()), end = " ")
+                print("|" + str(tile.getNumber()), end = " ")
         for tile in (self.tiles):
             x = tile.getX()
             y = tile.getY()       
@@ -103,12 +141,12 @@ class TilesBoard:
             x = tile.getX()
             y = tile.getY()        
             if ((x == 2) & (y == 1)):
-                print("|" + str(tile.getNumber()), end = " ")
+                print("|" + str(tile.getNumber()) + "|\n", end = " ")
         for tile in (self.tiles):
             x = tile.getX()
             y = tile.getY()        
             if ((x == 0) & (y == 2)):
-                print("|\n|" + str(tile.getNumber()), end = " ")
+                print("|" + str(tile.getNumber()), end = " ")
         for tile in (self.tiles):
             x = tile.getX()
             y = tile.getY()        
@@ -118,7 +156,7 @@ class TilesBoard:
             x = tile.getX()
             y = tile.getY()        
             if ((x == 2) & (y == 2)):
-                print("|" + str(tile.getNumber()) + "|", end = " ")
+                print("|" + str(tile.getNumber()) + "|\n", end = " ")
 
         return
                 
@@ -129,13 +167,8 @@ class TilesBoard:
             
         return moves
     
-tileBrd = TilesBoard
-tileBrd.printBoard(tileBrd)
-tileBrd.moveTiles(tileBrd)
-tileBrd.printBoard(tileBrd)
+tileBrd = TilesBoard()
 
-tileBrd.moveTiles(tileBrd)
-tileBrd.printBoard(tileBrd)
-
-tileBrd.moveTiles(tileBrd)
-tileBrd.printBoard(tileBrd)
+while (tileBrd.movesToSolve() != 0):
+    tileBrd.printBoard()
+    tileBrd.moveTiles()
